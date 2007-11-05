1cc2ddb0579a7d2faea5e2e2b270ca99a9561d57
raza <raza>
2007-11-05 15:54:56 +0000

    This new plugin associates the hexotic hexahedra to the GEOM shape.

--- src/HexoticPlugin/HexoticPlugin_Hexotic.cxx
+++ src/HexoticPlugin/HexoticPlugin_Hexotic.cxx
@@ -57,6 +57,11 @@ using namespace std;
 #include <cstdlib>
 #include <iostream>
 
+#include <BRepClass3d_SolidClassifier.hxx>
+#include <Bnd_Box.hxx>
+#include <BRepBndLib.hxx>
+#include <Precision.hxx>
+
 //=============================================================================
 /*!
  *  
@@ -172,6 +177,7 @@ static bool writeHexoticFile (ofstream &                      theFile,
   cout << "Creating Hexotic processed mesh file : " << Hexotic_In << endl;
 
   bool onlyExternalFaces = true;
+//  bool onlyExternalFaces = false;
   int nbVertices         = 0;
   int nbTriangles        = 0;
   const char* space      = "  ";
@@ -260,16 +266,52 @@ static bool writeHexoticFile (ofstream &                      theFile,
 }
 
 //=======================================================================
+//function : findShape
+//purpose  :
+//=======================================================================
+
+static TopoDS_Shape findShape(const SMDS_MeshNode *aNode[],
+                              TopoDS_Shape        aShape,
+                              const TopoDS_Shape  shape[],
+                              const double        box[][6],
+                              const int           nShape) {
+
+  Standard_Real PX, PY, PZ;
+  int iShape;
+
+  PX = ( aNode[0]->X() + aNode[1]->X() + aNode[2]->X() + aNode[3]->X() ) / 4.0;
+  PY = ( aNode[0]->Y() + aNode[1]->Y() + aNode[2]->Y() + aNode[3]->Y() ) / 4.0;
+  PZ = ( aNode[0]->Z() + aNode[1]->Z() + aNode[2]->Z() + aNode[3]->Z() ) / 4.0;
+  gp_Pnt aPnt(PX, PY, PZ);
+
+  BRepClass3d_SolidClassifier SC (aShape, aPnt, Precision::Confusion());
+  if ( not(SC.State() == TopAbs_IN) ) {
+    for (iShape = 0; iShape < nShape; iShape++) {
+      aShape = shape[iShape];
+      if ( not( PX < box[iShape][0] || box[iShape][1] < PX ||
+                PY < box[iShape][2] || box[iShape][3] < PY ||
+                PZ < box[iShape][4] || box[iShape][5] < PZ) ) {
+        BRepClass3d_SolidClassifier SC (aShape, aPnt, Precision::Confusion());
+        if (SC.State() == TopAbs_IN)
+          break;
+      }
+    }
+  }
+  return aShape;
+}
+
+//=======================================================================
 //function : readResult
 //purpose  : 
 //=======================================================================
 
 static bool readResult(string                           theFile,
                        SMESHDS_Mesh *                   theMesh,
-                       const TopoDS_Shape &             theShape,
                        map <int,const SMDS_MeshNode*> & theHexoticIdToNodeMap,
-                       const TCollection_AsciiString &  Hexotic_Out,
-                       int &                            nodeRefNumber)
+                       int &                            nodeRefNumber,
+                       const int                        nbShape,
+                       TopoDS_Shape                     tabShape[],
+                       double                           tabBox[][6])
 {
   // ---------------------------------
   // Read generated elements and nodes
@@ -279,12 +321,18 @@ static bool readResult(string                           theFile,
   ifstream fileRes(theFile.c_str());
   ASSERT(fileRes);
 
-  string token;
-  int shapeID = theMesh->ShapeToIndex( theShape );
-  int EndOfFile = 0, nbElem = 0, nField = 10, nbRef = 0, aHexoticNodeID = 0;
-  string theField;
-  int    tabRef [nField];
+  string token, theField;
+  int EndOfFile = 0, nbElem = 0, nField = 10, nbRef = 0;
+  int compoundID = theMesh->ShapeToIndex( theMesh->ShapeToMesh() );
+  int aHexoticNodeID = 0, shapeID, hexoticShapeID;
+  int IdShapeRef = 2;
+  int tabID[nbShape];
+  int tabRef [nField];
   map <string,int> mapField;
+  TopoDS_Shape aShape;
+
+  for (int i=0; i<nbShape; i++)
+    tabID[i] = 0;
 
   mapField["MeshVersionFormatted"] = 0; tabRef[0] = 0;
   mapField["Dimension"]            = 1; tabRef[1] = 0;
@@ -324,11 +372,11 @@ static bool readResult(string                           theFile,
       case 0: { // "MeshVersionFormatted"
         MESSAGE(token << " " << nbElem);
         break;
-        }
+      }
       case 1: { // "Dimension"
         MESSAGE("Mesh dimension " << nbElem << "D");
         break;
-        }
+      }
       case 2: { // "Vertices"
         MESSAGE("Read " << nbElem << " vertices");
         int aHexoticID;
@@ -341,11 +389,11 @@ static bool readResult(string                           theFile,
             fileRes >> coord[ iCoord ];
           fileRes >> dummy;
           aHexoticNode = theMesh->AddNode(coord[0], coord[1], coord[2]);
-          theMesh->SetNodeInVolume( aHexoticNode, shapeID );
+          theMesh->SetNodeInVolume( aHexoticNode, compoundID );
           theHexoticIdToNodeMap[ aHexoticID ] = aHexoticNode;
         }
         break;
-        }
+      }
       case 3: // "Edges"
       case 5: // "Quadrilaterals"
       case 6: { // "Hexahedra"
@@ -362,52 +410,64 @@ static bool readResult(string                           theFile,
             node[ iRef ] = itOnHexoticNode->second;
           }
           fileRes >> dummy;
+          shapeID = compoundID;
           switch (nField) {
             case 3: { // "Edges"
               aHexoticElement = theMesh->AddEdge( node[0], node[1] );
               break;
-              }
+            }
             case 5: { // "Quadrilaterals"
               aHexoticElement = theMesh->AddFace( node[0], node[1], node[2], node[3] );
               break;
-              }
+            }
             case 6: { // "Hexahedra"
-              aHexoticElement =
-              theMesh->AddVolume( node[0], node[1], node[2], node[3], node[4], node[5], node[6], node[7] );
-              break;
+              aHexoticElement = theMesh->AddVolume( node[0], node[3], node[2], node[1], node[4], node[7], node[6], node[5] );
+              if ( nbShape > 1 ) {
+                hexoticShapeID = dummy - IdShapeRef;
+                if ( tabID[ hexoticShapeID ] == 0 ) {
+                  if (iElem == 0)
+                    aShape = tabShape[0];
+                  aShape = findShape(node, aShape, tabShape, tabBox, nbShape);
+                  shapeID = theMesh->ShapeToIndex( aShape );
+                  tabID[ hexoticShapeID ] = shapeID;
+                }
+                else
+                  shapeID = tabID[ hexoticShapeID ];
               }
+              break;
+            }
           }
           theMesh->SetMeshElementOnShape( aHexoticElement, shapeID );
         }
         break;
-        }
+      }
       case 4: { // "Triangles"
         // MESSAGE("No action on token " << token << ", " << nbElem << " to read ") ;
         break;
-        }
+      }
       case 7: { // "Corners"
         // MESSAGE("No action on token " << token << ", " << nbElem << " to read ") ;
         for ( int iElem = 0; iElem < nbElem; iElem++ )
           fileRes >> dummy;
         break;
-        }
+      }
       case 8: { // "Ridges"
         // MESSAGE("No action on token " << token << ", " << nbElem << " to read ") ;
         for ( int iElem = 0; iElem < nbElem; iElem++ )
           fileRes >> dummy;
         break;
-        }
+      }
       case 9: { // "End"
         EndOfFile = 1;
         MESSAGE("End of " << theFile << " file");
         break;
-        }
+      }
       default: {
         MESSAGE("Unknown Token: " << token);
         // ASSERT(0);
-        }
       }
     }
+  }
   cout << endl;
   return true;
 }
@@ -467,7 +527,7 @@ static TCollection_AsciiString getTmpDir()
 bool HexoticPlugin_Hexotic::Compute(SMESH_Mesh&          theMesh,
                                      const TopoDS_Shape& theShape)
 {
-  bool Ok;
+  bool Ok = true;
   SMESHDS_Mesh* meshDS = theMesh.GetMeshDS();
   TCollection_AsciiString hexahedraMessage;
 
@@ -484,9 +544,28 @@ bool HexoticPlugin_Hexotic::Compute(SMESH_Mesh&          theMesh,
   _iShape++;
 
   if (_iShape == _nbShape ) {
+
+    // create bounding box for every shape
+
+    int iShape = 0;
+    TopoDS_Shape tabShape[_nbShape];
+    double tabBox[_nbShape][6];
+    Standard_Real Xmin, Ymin, Zmin, Xmax, Ymax, Zmax;
+
+    TopExp_Explorer expBox (meshDS->ShapeToMesh(), TopAbs_SOLID);
+    for (; expBox.More(); expBox.Next()) {
+      tabShape[iShape] = expBox.Current();
+      Bnd_Box BoundingBox;
+      BRepBndLib::Add(expBox.Current(), BoundingBox);
+      BoundingBox.Get(Xmin, Ymin, Zmin, Xmax, Ymax, Zmax);
+      tabBox[iShape][0] = Xmin; tabBox[iShape][1] = Xmax;
+      tabBox[iShape][2] = Ymin; tabBox[iShape][3] = Ymax;
+      tabBox[iShape][4] = Zmin; tabBox[iShape][5] = Zmax;
+      iShape++;
+    }
+
     SetParameters(_hypothesis);
 
-    cout << endl;
     cout << _name << " parameters :" << endl;
     cout << "    " << _name << " Segments Min Level = " << _hexesMinLevel << endl;
     cout << "    " << _name << " Segments Max Level = " << _hexesMaxLevel << endl;
@@ -502,11 +581,13 @@ bool HexoticPlugin_Hexotic::Compute(SMESH_Mesh&          theMesh,
     TCollection_AsciiString minl = " -minl ", maxl = " -maxl ", angle = " -ra ";
     TCollection_AsciiString in   = " -in ",   out  = " -out ";
     TCollection_AsciiString ignoreRidges = " -nr ", invalideElements = " -inv ";
+    TCollection_AsciiString subdom = " -sd ";
 
-    TCollection_AsciiString minLevel, maxLevel, sharpAngle;
+    TCollection_AsciiString minLevel, maxLevel, sharpAngle, mode;
     minLevel = _hexesMinLevel;
     maxLevel = _hexesMaxLevel;
     sharpAngle = _hexoticSharpAngleThreshold;
+    mode = 4;
 
     map <int,int> aSmdsToHexoticIdMap;
     map <int,const SMDS_MeshNode*> aHexoticIdToNodeMap;
@@ -521,7 +602,9 @@ bool HexoticPlugin_Hexotic::Compute(SMESH_Mesh&          theMesh,
       run_Hexotic +=  invalideElements;
 
     run_Hexotic += angle + sharpAngle + minl + minLevel + maxl + maxLevel + in + Hexotic_In + out + Hexotic_Out;
+    run_Hexotic += subdom + mode;
 
+    cout << endl;
     cout << "Hexotic command : " << run_Hexotic << endl;
 
     OSD_File( Hexotic_In  ).Remove();
@@ -542,7 +625,8 @@ bool HexoticPlugin_Hexotic::Compute(SMESH_Mesh&          theMesh,
 
     ifstream fileRes( Hexotic_Out.ToCString() );
     if ( ! fileRes.fail() ) {
-      Ok = readResult( Hexotic_Out.ToCString(), meshDS, theShape, aHexoticIdToNodeMap, Hexotic_Out, _nodeRefNumber );
+      Ok = readResult( Hexotic_Out.ToCString(), meshDS, aHexoticIdToNodeMap, _nodeRefNumber,
+                       _nbShape, tabShape, tabBox );
       hexahedraMessage = "complete";
     }
     else {
